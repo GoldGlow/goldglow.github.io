d3026bd305947e5133bc0c1f93c8f9ac7d0bd938
JeanMarc <TacticianJean@gmail.com>
2019-09-14 21:20:20 -0400

    Added the manual scripts section
    Fixes coming soon, probably.

--- guide/js/navigation.js
+++ guide/js/navigation.js
@@ -3,6 +3,21 @@ function setPage(title, page){
 	$(".content").load(page);
 }
 
+function toggleNPCFunctions(){
+	if($("#functionsList").css("display")==="none"){
+		$("#functionsList").css("display","block");
+	}
+	else{
+		$("#functionsList").css("display","none");
+	}
+}
+
+function setFunctionValues(title, fileName){
+	$("#scriptTitle").html(title);
+	$("#scriptConsole").load("pages/console/"+fileName+".txt");
+	$("#scriptInfo").load("pages/info/"+fileName+".txt");
+}
+
 $(document).ready(function(){
   $("#intro").click(function(){
     setPage("Introduction", "pages/intro.txt");
@@ -12,5 +27,50 @@ $(document).ready(function(){
   });
   $("#manualScripting").click(function(){
 	setPage("Manual Scripting<br/><span style=\"font-size:16px;\">(It's more intimidating than it really is)</span>", "pages/manualScripting.txt");
+  });
+  $("#prewrittenFunctions").click(function(){
+	  setPage("Prewritten functions", "pages/prewritten.txt");
+  });
+  $("#npcFunctions").click(function(){
+		toggleNPCFunctions();
+  });
+  $("#achievement").click(function(){
+		setFunctionValues("Achievement", "achievement");
+  });
+  $("#playSong").click(function(){
+		setFunctionValues("playSong", "playSong");
+  });
+  $("#setCurrentSong").click(function(){
+		setFunctionValues("setCurrentSong", "setCurrentSong");
+  });
+  $("#checkRoute").click(function(){
+		setFunctionValues("checkRoute", "checkRoute");
+  });
+  $("#updatePos").click(function(){
+		setFunctionValues("updatePos", "updatePos");
+  });
+  $("#doubleNPCBattle").click(function(){
+		setFunctionValues("createDoubleNPCBattle", "doubleNPCBattle");
+  });
+  $("#npcBattle").click(function(){
+		setFunctionValues("createNPCBattle", "npcBattle");
+  });
+  $("#notification").click(function(){
+		setFunctionValues("dialogNotification", "notification");
+  });
+  $("#losDialog").click(function((){
+		setFunctionValues("LoSDialog", "losDialog");
+  });
+  $("#blockDialog").click(function(){
+		setFunctionValues("openBlockDialog", "blockDialog")
+  });
+  $("#repeatableQuest").click(function(){
+		setFunctionValues("repeatableQuestDialogs","repeatableQuest");
+  });
+  $("#blockLight").click(function(){
+		setFunctionValues("setBlockLight","blockLight");
+  });
+  $("#blockModel").click(function(){
+		setFunctionValues("setBlockModel","blockModel");
   });
 });
